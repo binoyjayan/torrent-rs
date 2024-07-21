6f5a80bfe8e4af82b7e239e270239ce41102bc59
Binoy Jayan <binoyjayan@gmail.com>
2024-07-21 09:16:59 -0700

    decode bencoded lists

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,44 @@
 use clap::Parser;
-use serde_bencode;
+use serde_bencode::value::Value as BencodeValue;
+use serde_json::Value as JsonValue;
 
 mod cli;
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    match encoded_value.chars().next().unwrap() {
-        'i' => {
-            // Numbers are encoded as i<number>e
-            let number: i64 = serde_bencode::from_bytes(encoded_value.as_bytes()).unwrap();
-            serde_json::Value::Number(serde_json::Number::from(number))
+fn decode_bencoded_value(value: BencodeValue) -> anyhow::Result<JsonValue> {
+    Ok(match value {
+        BencodeValue::Bytes(b) => {
+            let string = String::from_utf8(b)?;
+            JsonValue::String(string)
         }
-        'l' => {
-            // Lists are encoded as l<value1><value2>...e
-            serde_json::Value::Null
+        BencodeValue::Int(i) => JsonValue::Number(i.into()),
+        BencodeValue::List(l) => {
+            let array = l
+                .into_iter()
+                .map(decode_bencoded_value)
+                .collect::<anyhow::Result<Vec<serde_json::Value>>>()?;
+            JsonValue::Array(array)
         }
-        'd' => {
-            // Dictionaries are encoded as d<key1><value1><key2><value2>...e
-            serde_json::Value::Null
-        }
-        digit => {
-            // Strings are encoded as <length>:<string>
-            if digit.is_ascii_digit() {
-                let colon_index = encoded_value.find(':').unwrap();
-                let number_string = &encoded_value[..colon_index];
-                let number = number_string.parse::<i64>().unwrap();
-                let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-                serde_json::Value::String(string.to_string())
-            } else {
-                panic!("Unhandled encoded value: {}", encoded_value)
-            }
-        }
-    }
+        _ => anyhow::bail!("Unhandled encoded value: {:?}", value),
+    })
 }
 
-fn main() {
+fn decode_bencoded_str(encoded_value: &str) -> anyhow::Result<serde_json::Value> {
+    let value: BencodeValue = serde_bencode::from_str(encoded_value)?;
+
+    decode_bencoded_value(value)
+}
+
+fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
 
     match cli.command {
         Some(cli::Commands::Decode { encoded_value }) => {
-            let decoded_value = decode_bencoded_value(&encoded_value);
+            let decoded_value = decode_bencoded_str(&encoded_value)?;
             println!("{}", decoded_value);
         }
         None => {
-            println!("unknown command");
+            anyhow::bail!("unknown command");
         }
     }
+    Ok(())
 }
